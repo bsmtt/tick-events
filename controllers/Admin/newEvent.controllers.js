edb434b9075a61eb99c1f45e6881984eb804ef94
bsm2 <basmatalaat2@gmail.com>
2024-11-01 00:40:59 +0200

    feat: edit event

--- controllers/Admin/newEvent.controllers.js
+++ controllers/Admin/newEvent.controllers.js
@@ -27,6 +27,30 @@ class NewEventController {
       });
     }
   };
+
+  update = async (req, res) => {
+    try {
+      let id = req.params.id;
+      const data = await getHostSchema(req).validateAsync(req.body);
+      const newEvent = await NewEvent.updateOne({ _id: id }, data);
+
+      res.status(200).json({
+        status: true,
+        message: i18n.__("event_updated"),
+        data: host,
+      });
+    } catch (err) {
+      let message =
+        err instanceof Joi.ValidationError
+          ? err.details[0].message
+          : err.message;
+      res.status(422).json({
+        status: false,
+        data: message,
+        message: message,
+      });
+    }
+  };
 }
 
 module.exports = new NewEventController();
